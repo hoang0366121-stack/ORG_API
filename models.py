fd90201f58915132b455a4a536f7fe080615a59c
Hoàng Thanh Hiệp <hoangthanhhiep@MacBook-Air-cua-Hoang.local>
2025-12-04 16:36:38 +0700

    Update code

---
+++ models.py
@@ -0,0 +1,42 @@
+from sqlalchemy import Column, Integer, String, ForeignKey , Enum , Date
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Department(Base):
+    __tablename__ = "departments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    parent_id = Column(Integer, ForeignKey("departments.id"), nullable=True)
+
+    parent = relationship("Department", remote_side=[id], backref="children")
+    users = relationship("User", back_populates="department")
+
+
+class Position(Base):
+    __tablename__ = "positions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    users = relationship("User", back_populates="position")
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+#code thêm
+
+    sex = Column(
+        Enum ("Nam", "Nu",name ="users_sex" ),
+        nullable = False
+    )
+    date_of_birth = Column(Date, nullable=False )
+    start_date = Column(Date, nullable=False  )
+                
+    department_id = Column(Integer, ForeignKey("departments.id"))
+    position_id = Column(Integer, ForeignKey("positions.id"))
+
+    department = relationship("Department", back_populates="users")
+    position = relationship("Position", back_populates="users")
---
+++ schemas.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+# Position
+class PositionBase(BaseModel):
+    title: str
+
+class PositionCreate(PositionBase):
+    pass
+
+class Position(PositionBase):
+    id: int
+    class Config:
+        from_attributes = True
+
+# User
+class UserBase(BaseModel):
+    name: str
+    sex: str
+    start_date : date
+    department_id: Optional[int]
+    position_id: Optional[int]
+
+class UserCreate(UserBase):
+    pass
+
+class User(UserBase):
+    id: int
+    sex: str
+    start_date: date
+    position: Optional[Position]
+    class Config:
+        from_attributes = True
+
+# Department
+class DepartmentBase(BaseModel):
+    name: str
+    parent_id: Optional[int] = None
+
+class DepartmentCreate(DepartmentBase):
+    pass
+
+class Department(DepartmentBase):
+    id: int
+    children: List["Department"] = Field(default_factory=list)
+    users: List[User] = Field(default_factory=list)
+
+    class Config:
+        from_attributes = True
+
